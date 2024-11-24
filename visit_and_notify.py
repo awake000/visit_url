495dd4459ed2344683b45fa1f7978e073cee623c
awake000 <81493284+awake000@users.noreply.github.com>
2024-11-24 13:38:16 +0800

    Update visit_and_notify.py

--- visit_and_notify.py
+++ visit_and_notify.py
@@ -17,8 +17,9 @@ def visit_url():
         #return f"Visited URL successfully, Status Code: {response_1.status_code}"
         if response.status_code == 200 and response_1.status_code == 200 :
             return f"推送更新成功\ntxt更新成功:ok\nm3u更新成功:ok"
-        except requests.RequestException as e:
-        return f"推送更新失败\ntxt更新成功:false\nm3u更新成功:false\n失败原因:{str(e)}"
+        else:
+            except requests.RequestException as e:
+            return f"推送更新失败\ntxt更新成功:false\nm3u更新成功:false\n失败原因:{str(e)}"
 
 def send_email(text):
     # 邮件服务器地址和端口号
